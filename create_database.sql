e4c4cb4c64310f12ffb2c7af7da533508ddbc510
jerev7 <verdet.jeremie@gmail.com>
2019-10-07 13:13:40 +0200

    added some practice filse to the gitignore and added sql file which will create database

---
+++ create_database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Products1 (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	category VARCHAR(100) NOT NULL,
+	brand VARCHAR(100), 
+	conditioning VARCHAR(100)
+	labels VARCHAR(100)   
+	)
--- tuto.py
+++ tuto.py
@@ -1,8 +1,7 @@
 #! /usr/bin/env python3
 #coding:utf-8
 
-from PySide2.QtWidgets import (QApplication, QPushButton, QDialog, QLineEdit, QVBoxLayout)
-from time import *
+from PySide2.QtWidgets import (QApplication, QPushButton, QDialog, QLineEdit, QVBoxLayout, QMessageBox)
 
 
 
@@ -12,7 +11,8 @@ class Form(QDialog):
         super(Form, self).__init__(parent)
         
         # Create widgets
-        self.edit = QLineEdit("Write my name here...")
+        self.edit = QLineEdit()
+        self.edit.setPlaceholderText("Write your name here...")
         self.button = QPushButton("Show Greetings")
         # Create layout and add widgets
         layout = QVBoxLayout()
@@ -25,9 +25,28 @@ class Form(QDialog):
 
     # Greets the user
     def greetings(self):
-        print(f"Go fuck yourself {self.edit.text()}")
+        #print(f"hey {self.edit.text()}")
+        self.resultat = Resultat(self)
+        self.resultat.show()
+        #QMessageBox.information(self, "salut", "ça va", QMessageBox.Close)
 
+class Resultat(QDialog):
 
+    def __init__(self, parent=None):
+        super(Resultat, self).__init__(parent)
+        
+        # Create widgets
+        self.edit = QLineEdit()
+        self.edit.setPlaceholderText("deuxieme fenetre...")
+        self.button = QPushButton("recherche")
+        # Create layout and add widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+        # Set dialog layout
+        self.setLayout(layout)
+        # Add button signal to greetings slot
+        #self.button.clicked.connect(self.greetings)
 
 if __name__ == '__main__':
     # Create the Qt Application
