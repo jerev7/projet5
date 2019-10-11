29476d59bf8a45d09eb6cee203de963af266f803
jerev7 <verdet.jeremie@gmail.com>
2019-10-11 15:25:24 +0200

    added the script to get data from API

--- create_database.sql
+++ create_database.sql
@@ -1,7 +1,12 @@
 CREATE TABLE Products1 (
-	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	category VARCHAR(100) NOT NULL,
-	brand VARCHAR(100), 
-	conditioning VARCHAR(100)
-	labels VARCHAR(100)   
-	)
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    category VARCHAR(100) NOT NULL,
+    brand VARCHAR(100), 
+    conditioning VARCHAR(100),
+    labels VARCHAR(100),
+    ingredients_origin VARCHAR(100),
+    manufacturing_place VARCHAR(100),
+    PRIMARY KEY (id)
+
+    )
+ENGINE=INNODB;
