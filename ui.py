5c5d1da01e6a8d319bc7a0f204227c0d0edbabeb
jerev7 <verdet.jeremie@gmail.com>
2020-01-09 16:55:42 +0100

    tried to add sql query

--- ui.py
+++ ui.py
@@ -3,13 +3,14 @@
 
 from PySide2.QtWidgets import (QApplication, QPushButton, QDialog, QLineEdit, QVBoxLayout, QMessageBox)
 from PySide2 import QtWidgets, QtCore, QtGui
-
+import mysql.connector
 
 
 class Menu(QDialog):
 
     def __init__(self, parent=None):
         super(Menu, self).__init__(parent)
+
 
         self.createMenu()
         
@@ -47,6 +48,7 @@ class Resultat(QDialog):
 
     def __init__(self, parent=None):
         super(Resultat, self).__init__(parent)
+
         
         # Create widgets
         self.text = QtWidgets.QLabel("Choose a category below")
@@ -54,9 +56,26 @@ class Resultat(QDialog):
         self.mycombo = QtWidgets.QComboBox()
         self.button = QPushButton("recherche")
         # Create layout and add widgets
+        
+        """le probleme est avec le code qui suit"""
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="jerev7",
+            passwd="Sally_95540",
+            database="openfoodfacts"
+        )
+
+        mycursor = mydb.cursor()
+        sql_id = "SELECT id FROM Category"
+        sql_name = "SELECT name FROM Category"
+        mycursor.execute(sql_id)
+        result = mycursor.fetchall()
+        for x in result:
+            self.mycombo.addItem(x)
+
+
         layout = QVBoxLayout()
         
-        # layout.addWidget(self.mycombo)
         layout.addWidget(self.text)
         layout.addWidget(self.mycombo)
         layout.addWidget(self.button)
@@ -64,6 +83,8 @@ class Resultat(QDialog):
         self.setLayout(layout)
         # Add button signal to greetings slot
         #self.button.clicked.connect(self.greetings)
+       
+
 
 if __name__ == '__main__':
 
