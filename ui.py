218f7b7f4bf78a404037fd5fded8589ea48ccb72
jerev7 <verdet.jeremie@gmail.com>
2020-03-16 16:25:54 +0100

    saved products are displayed in table3

--- ui.py
+++ ui.py
@@ -295,8 +295,8 @@ class Saved_products(QDialog):
     def __init__(self, parent=None):
         super(Saved_products, self).__init__(parent)
 
-        self.mytable3 = QtWidgets.QTableWidget(1, 4)
-        self.mytable3.setHorizontalHeaderLabels(("Produit sélectionné;Nutriscore;Magasins;Lien vers le site web").split(";"))
+        self.mytable3 = QtWidgets.QTableWidget(0, 4)
+        self.mytable3.setHorizontalHeaderLabels(("Produit sélectionné;Substitution choisi;Magasins;Lien vers le site web").split(";"))
         header = self.mytable3.horizontalHeader()
         header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
         header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
@@ -305,6 +305,50 @@ class Saved_products(QDialog):
         # header.setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
         # header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
         # header.setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
+        sql1 = "SELECT product_name FROM Product WHERE id = %s"
+        sql2 = "SELECT product_name, stores, url FROM Product WHERE id = %s"
+        sql3 = "SELECT product_selected_id FROM Product_saved"
+        sql4 = "SELECT substitution_product_id FROM Product_saved"
+
+        self.mydb = mysql.connector.connect(
+        host="localhost",
+        user="jerev7",
+        passwd="Sally_95540",
+        database="openfoodfacts"
+        )
+
+        self.mycursor = self.mydb.cursor()
+
+        self.mycursor.execute(sql3)
+        result3 = self.mycursor.fetchall()
+        self.mycursor.execute(sql4)
+        result4 = self.mycursor.fetchall()
+
+        row_nbr = 0
+        for x in result3:
+            print(x)
+            self.mycursor.execute(sql1, x)
+            self.mytable3.insertRow(row_nbr)
+            result1 = self.mycursor.fetchall()
+            for y in result1:
+                prod_name = QtWidgets.QTableWidgetItem(y[0])
+                self.mytable3.setItem(row_nbr, 0, prod_name)
+            row_nbr += 1
+        row_nbr = 0
+        for x in result4:
+            self.mycursor.execute(sql2, x)
+            result2 = self.mycursor.fetchall()
+            for y in result2:
+                subs_name = QtWidgets.QTableWidgetItem(y[0])
+                subs_stores = QtWidgets.QTableWidgetItem(y[1])
+                subs_url = QtWidgets.QTableWidgetItem(y[2])
+                self.mytable3.setItem(row_nbr, 1, subs_name)
+                self.mytable3.setItem(row_nbr, 2, subs_stores)
+                self.mytable3.setItem(row_nbr, 3, subs_url)
+            row_nbr += 1
+
+
+
 
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.mytable3)
